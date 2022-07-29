53c3892e9578cd37161c9d47f40fabe5d97afed4
Scott Rinaberger <scottrinaberger@gmail.com>
2022-07-29 15:58:06 -0500

    current code for josh

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,19 @@
 // Assignment code here
 
 
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var getLength = function() {
+    /* ask for length of password */
+    var promptLength = window.prompt('How many characters would you like your password to be? Please choose between 8 and 120 characters.');
+  }
+};
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -33,7 +42,3 @@ var number = [0,1,2,3,4,5,6,7,8,9];
 var specialCharacter = [
   "!","@","#","$","%","^","&","?"
 ];
-
-
-/* RUN PASSWORD GENERATOR */
-Array
