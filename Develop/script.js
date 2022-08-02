10fc522e68761c0472cd54d1faea6f5a7695a606
Scott Rinaberger <scottrinaberger@gmail.com>
2022-08-01 19:20:38 -0500

    realigned ifs

--- Develop/script.js
+++ Develop/script.js
@@ -9,108 +9,106 @@ function generatePassword() {
   // Ask user for length of password.
   var promptLength = window.prompt('How many characters would you like your password to be?', "Choose between 8-128 characters");
   // Validate input of promptLength to be between 8 and 128.
-  if (promptLength === "" || promptLength === null) {
-    window.alert(" Please provide a number between 8 and 128.");
-    return generatePassword();
-  }
-  else if (promptLength < 8 || promptLength > 128) {
-    window.alert("Please provide a number between 8 and 128.");
-    return generatePassword();
-  }
-  else if (promptLength >= 8 || promptLength <= 128) {
-    window.alert("Your password will be " + promptLength + " characters long."); 
-    console.log("promptLength = " + promptLength);       
-  }
-  else {
-    window.alert(" Please provide a number between 8 and 128.");
-    return generatePassword();
-  };
+    if (promptLength === "" || promptLength === null) {
+      window.alert(" Please provide a number between 8 and 128.");
+      return generatePassword();
+    }
+    else if (promptLength < 8 || promptLength > 128) {
+      window.alert("Please provide a number between 8 and 128.");
+      return generatePassword();
+    }
+    else if (promptLength >= 8 || promptLength <= 128) {
+      window.alert("Your password will be " + promptLength + " characters long."); 
+      console.log("promptLength = " + promptLength);       
+    }
+    else {
+      window.alert(" Please provide a number between 8 and 128.");
+      return generatePassword();
+    };
   
   // Ask if uppercase letters should be included.
   var promptUppercase = window.prompt('Would you like to include uppercase letters?' , "Yes or No?");
   // Validate input of promptUpperCase to be either "Yes" or "No"
-    
-  if (promptUppercase === "yes" || promptUppercase === "YES" || promptUppercase === "Yes") {
+    if (promptUppercase === "yes" || promptUppercase === "YES" || promptUppercase === "Yes") {
       window.alert("Uppercase Letters will be included.");
       promptUppercase = "Yes";
       console.log("promptUppercase = " + promptUppercase)       
-  }
-  else if (promptUppercase ==="no" || promptUppercase === "NO" || promptUppercase === "No") {
+    }
+    else if (promptUppercase ==="no" || promptUppercase === "NO" || promptUppercase === "No") {
       window.alert("Uppercase Letters will NOT be included.");
       promptUppercase = "No"; 
       console.log("promptUppercase = " + promptUppercase);           
-  }
-  else {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  };
+    }
+    else {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    };
 
   // Ask if lowercase letters should be included.
   var promptLowercase = window.prompt('Would you like to include lowercase letters?' , "Yes or No?");
   // Validate input of promptUpperCase to be either "Yes" or "No"
-  if (promptLowercase === "" || promptLowercase === null) {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  }
-  else if (promptLowercase === "yes" || promptLowercase === "YES" || promptLowercase === "Yes") {
+    if (promptLowercase === "" || promptLowercase === null) {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    }
+    else if (promptLowercase === "yes" || promptLowercase === "YES" || promptLowercase === "Yes") {
       window.alert("Lowercase letters will be included.");
       promptLowercase = "Yes";
       console.log("promptLowercase = " + promptLowercase)       
-  }
-  else if (promptLowercase ==="no" || promptLowercase === "NO" || promptLowercase === "No") {
+    }
+    else if (promptLowercase ==="no" || promptLowercase === "NO" || promptLowercase === "No") {
       window.alert("Lowercase letters will NOT be included.");
       promptLowercase = "No";  
       console.log("promptLowercase = " + promptLowercase);       
-  }
-  else {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  };
+    }
+    else {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    };
   
   // Ask if numbers should be included.
   var promptNumber = window.prompt('Would you like to include numbers?' , "Yes or No?");
-  // Validate input of promptUpperCase to be either "Yes" or "No"
-  if (promptNumber === "" || promptNumber === null) {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  }
-  else if (promptNumber === "yes" || promptNumber === "YES" || promptNumber === "Yes") {
+    // Validate input of promptUpperCase to be either "Yes" or "No"
+    if (promptNumber === "" || promptNumber === null) {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    }
+    else if (promptNumber === "yes" || promptNumber === "YES" || promptNumber === "Yes") {
       window.alert("Special characters will be included.");
       promptNumber = "Yes";
       console.log("promptNumber = " + promptNumber);       
-  }
-  else if (promptNumber ==="no" || promptNumber === "NO" || promptNumber === "No") {
+    }
+    else if (promptNumber ==="no" || promptNumber === "NO" || promptNumber === "No") {
       window.alert("Special characters will NOT be included.");
       promptNumber = "No";
       console.log("promptNumber = " + promptNumber); 
-  }
-  else {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  };
+    }
+    else {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    };
    
   // Ask if special characters should be included.
   var promptSpecialChar = window.prompt('Would you like to include special characters?' , "Yes or No?");
-  // Validate input of promptUpperCase to be either "Yes" or "No"
-  if (promptSpecialChar === "" || promptSpecialChar === null) {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  }
-  else if (promptSpecialChar === "yes" || promptSpecialChar === "YES" || promptSpecialChar === "Yes") {
+    // Validate input of promptUpperCase to be either "Yes" or "No"
+    if (promptSpecialChar === "" || promptSpecialChar === null) {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    }
+    else if (promptSpecialChar === "yes" || promptSpecialChar === "YES" || promptSpecialChar === "Yes") {
       window.alert("Special characters will be included.");
       promptSpecialChar = "Yes";
       console.log("promptSpecialChar = " + promptSpecialChar); 
-  }
-  else if (promptSpecialChar ==="no" || promptSpecialChar === "NO" || promptSpecialChar === "No" ) {
+    }
+    else if (promptSpecialChar ==="no" || promptSpecialChar === "NO" || promptSpecialChar === "No" ) {
       window.alert("Special characters will NOT be included.");
       promptSpecialChar = "No";
       console.log("promptSpecialChar = " + promptSpecialChar); 
-  }
-  else {
-    window.alert(" Please answer Yes or No.");
-    return generatePassword();
-  };
-  console.log(generatePassword);
+    }
+    else {
+      window.alert(" Please answer Yes or No.");
+      return generatePassword();
+    };  
 };
 
 
